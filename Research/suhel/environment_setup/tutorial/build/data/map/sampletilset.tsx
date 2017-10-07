6f276f6354b1c5711c85ae70ffcf485eff1a6d98
Suhel <suhel.mehta@sjsu.edu>
2017-10-07 03:44:51 -0700

    Add files via upload

---
+++ Research/suhel/environment_setup/tutorial/build/data/map/sampletilset.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset name="area01_level_tiles" tilewidth="32" tileheight="32" tilecount="198" columns="22">
+ <image source="720px-SMB3UnusedLevel12.png" width="720" height="314"/>
+</tileset>
---
+++ Research/suhel/environment_setup/tutorial/build/js/resources.js
@@ -0,0 +1,17 @@
+game.resources = [
+    {
+        "name": "720px-SMB3UnusedLevel12",
+        "type": "image",
+        "src": "data/img/720px-SMB3UnusedLevel12.png"
+    },
+    {
+        "name": "area01",
+        "type": "tmx",
+        "src": "data/map/area01.tmx"
+    },
+    {
+        "name": "sampletilset",
+        "type": "tsx",
+        "src": "data/map/sampletilset.tsx"
+    }
+];
