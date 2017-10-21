df6751eb7fc830a38c7c3fedb4741f417e312836
kimtani90 <kimtani90@gmail.com>
2017-10-20 22:01:28 -0700

    tiled map

---
+++ Research/dishant/SampleTiledGame/Resources.js
@@ -0,0 +1,10 @@
+var resources = [{
+  name: "levelSprites",
+  type: "image",
+  src: "levelSprites.jpg"
+},
+{
+  name: "level1",
+  type: "tmx",
+  src: "level1.tmx"
+}];
---
+++ Research/dishant/SampleTiledGame/Tile1.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset name="Tile1" tilewidth="16" tileheight="16" tilecount="1131" columns="39">
+ <image source="../../Downloads/Tile1.jpg" width="637" height="477"/>
+</tileset>
