f9704b2049ee1074ccf706f079e3691af4701d85
Dipro Chowdhury <achowdh1@mail.sfsu.edu>
2017-10-27 09:56:23 -0700

    state changed to play

--- Design/Game Source Code/js/game.js
+++ Design/Game Source Code/js/game.js
@@ -37,7 +37,7 @@ var game = {
     loaded: function ()    {
 
          // set the "Play/Ingame" Screen Object
-        me.state.set(me.state.MENU, new game.TitleScreen());
+        //me.state.set(me.state.MENU, new game.TitleScreen());
 
         // set the "Play/Ingame" Screen Object
         me.state.set(me.state.PLAY, new game.PlayScreen());
@@ -81,6 +81,6 @@ var game = {
         });
 
          // display the menu title
-         me.state.change(me.state.MENU);
+         me.state.change(me.state.PLAY);
     }
 };
