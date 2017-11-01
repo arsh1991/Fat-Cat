8b2f42f8ba9297ed9f23d40e0fd3036a3e220155
arsh1991 <91singh.arshdeep@gmail.com>
2017-10-31 23:51:54 -0700

    Created a new tileset

---
+++ Resources/Tileset/level3.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset name="level" tilewidth="32" tileheight="32" tilecount="510" columns="30">
+ <image source="../../../Downloads/GameAssets/kenney_platformerpack_industrial/Tilesheet/platformerPack_industrial_tilesheet.png" width="980" height="560"/>
+</tileset>
