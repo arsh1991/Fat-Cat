3e0f7d443b964a7ade34b90d496ac17c198a5d49
Dipro Chowdhury <achowdh1@mail.sfsu.edu>
2017-11-07 00:46:33 -0800

    background changed

--- Design/Game Source Code/js/screens/title_screen.js
+++ Design/Game Source Code/js/screens/title_screen.js
@@ -4,28 +4,28 @@ game.TitleScreen = me.ScreenObject.extend({
    */
   onResetEvent : function () {
     // title screen
-    //var backgroundImage = new me.Sprite(0, 0, {
-          //  image: me.loader.getImage('title_screen'),
-       // }
-    //);
+    var backgroundImage = new me.Sprite(0, 0, {
+            image: me.loader.getImage('background'),
+        }
+    );
 
     // position and scale to fit with the viewport size
-    //backgroundImage.anchorPoint.set(0, 0);
-   // backgroundImage.scale(me.game.viewport.width / backgroundImage.width, me.game.viewport.height / backgroundImage.height);
+   backgroundImage.anchorPoint.set(0, 0);
+   backgroundImage.scale(me.game.viewport.width / backgroundImage.width, me.game.viewport.height / backgroundImage.height);
    // clear the background
    //me.game.world.addChild(new me.ColorLayer("background", "rgba(210, 104, 40, 255)"), 0);
    me.save.add({ hiscore : 0,second:0,third:0});
    console.log(JSON.stringify(me.save));
 
-   var sprite = new me.ImageLayer(0, 0, {
-    image : "background",
-    framewidth : 64,
-    frameheight : 64,
-    anchorPoint : new me.Vector2d(0.5, 0.5)
-});
-   me.game.world.addChild(sprite);
+   //var sprite = new me.ImageLayer(0, 0, {
+    //image : "background",
+    //framewidth : 64,
+    //frameheight : 64,
+    //anchorPoint : new me.Vector2d(0.5, 0.5)
+//});
+   me.game.world.addChild(backgroundImage);
    //this.SelectPlayerButton = new game.UI.ButtonUI(500, 175, "green", "Select Player");
-   this.PlayButton = new game.UI.ButtonUI(500, 250,"blue","Play Game");
+   this.PlayButton = new game.UI.ButtonUI(495, 500,"blue","Play Game");
    //me.game.world.addChild(this.SelectPlayerButton);
    me.game.world.addChild(this.PlayButton);
    me.game.world.addChild(new game.TitleScreen.Message(me.save.hiscore,me.save.second,me.save.third));
